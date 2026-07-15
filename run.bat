1b694e13c9003f516aa5bca749cde9177183e5ab
mantav-garg <mantavgarg12@gmail.com>
2026-07-15 17:38:09 +0530

    regular update

--- run.bat
+++ run.bat
@@ -7,7 +7,7 @@ call deactivate
 git add .
 git diff --cached --quiet
 if errorlevel 1 (
-    git commit -m "regular svg update"
+    git commit -m "regular update"
     git push origin main
 ) else (
     echo No changes to commit.
